988c69c2b7c824d971c5cf6d4ce1d6df56ebc667
shakib522 <shakibhasann522@gmail.com>
2024-01-21 12:02:53 +0600

    updated

---
+++ src/main/kotlin/leetcode/724.FindPivotIndex.kt
@@ -0,0 +1,35 @@
+package leetcode
+
+import java.util.*
+
+fun main(){
+    val scanner= Scanner(System.`in`)
+    val n=scanner.nextInt()
+    val array=IntArray(n)
+    for (i in 0 ..<n){
+        array[i]=scanner.nextInt()
+    }
+    println(pivotIndex(array))
+}
+
+fun pivotIndex(nums: IntArray): Int {
+    val array=IntArray(nums.size+1)
+    array[0]=0
+    for (i in nums.indices){
+        array[i+1]=nums[i]
+    }
+    for (i in 1 ..< array.size){
+        array[i]=array[i-1]+array[i]
+    }
+    for (i in array){
+        println("array: $i")
+    }
+    for (i in 1 ..< array.size){
+        val left=array[i-1]
+        val right=array[array.size-1]-array[i]
+        if(left==right){
+            return i-1
+        }
+    }
+    return -1
+}
