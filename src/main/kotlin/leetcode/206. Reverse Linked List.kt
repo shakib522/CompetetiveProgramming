6528c25052fb3fb865ce5bbf4c82e173b96ec89a
shakib522 <shakibhasann522@gmail.com>
2024-03-03 11:28:45 +0600

    solved

---
+++ src/main/kotlin/leetcode/206. Reverse Linked List.kt
@@ -0,0 +1,33 @@
+package leetcode
+
+
+fun append(head: ListNode?, item: Int): ListNode {
+    var currentNode: ListNode? = head
+
+    val newNode = ListNode(item)
+    if (head == null) {
+        return newNode
+    }
+    while (currentNode?.next != null) {
+        currentNode = currentNode.next
+    }
+    currentNode?.next = newNode
+    return head
+}
+
+fun reverseList(head: ListNode?): ListNode? {
+    if(head==null){
+        return null
+    }
+    var currNode = head
+    val array = mutableListOf<Int>()
+    while (currNode != null) {
+        array.add(currNode.valu)
+        currNode=currNode.next
+    }
+    var temp:ListNode? = ListNode(array[array.size-1])
+    for (i in array.size-2 downTo  0) {
+        append(temp, array[i])
+    }
+    return temp
+}
