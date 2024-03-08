1e8b1d7b808ea1307c9d80245184f171a91395ba
shakib522 <shakibhasann522@gmail.com>
2024-03-08 07:35:20 +0600

    solved

---
+++ src/main/kotlin/leetcode/704.Binary Search.kt
@@ -0,0 +1,23 @@
+package src.main.kotlin.leetcode
+
+fun main(){
+
+}
+
+fun search(nums: IntArray, target: Int): Int {
+    return binarySearch(0,nums.size-1,nums,target)
+}
+fun binarySearch(start:Int,end:Int,nums:IntArray,target:Int):Int{
+    if(start>end){
+        return -1
+    }
+
+    val mid=start+(end-start)/2
+    if (nums[mid]==target){
+        return mid
+    }
+    if (nums[mid]>target){
+        return binarySearch(start,mid-1,nums,target)
+    }
+    return binarySearch(mid+1,end,nums,target)
+}
