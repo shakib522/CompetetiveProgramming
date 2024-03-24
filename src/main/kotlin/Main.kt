0ca7d3576e7d1bbc17316d18326ab376f94ca936
shakib522 <shakibhasann522@gmail.com>
2024-03-24 23:26:06 +0600

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,16 @@ import java.util.*
 fun main() {
     val scanner=Scanner(System.`in`)
     val n=scanner.nextInt()
+    scanner.nextLine()
     val numArray=IntArray(n)
+    val string= scanner.nextLine()
+    val ban=Array(n){" "}
     for (i in 0..<n){
-        numArray[i]=scanner.nextInt()
+        ban[i]=scanner.nextLine()
     }
-   increasingTriplet(numArray)
+    //println(mostCommonWord(string,ban))
 }
 
-fun increasingTriplet(nums: IntArray): Boolean {
-    var target=Int.MAX_VALUE
-    var min=Int.MAX_VALUE
-    for (n in nums){
-        if(n>target) return true
-        if(n>min) target=n
-        if(n<min) min=n
-    }
-    return false
-}
+//fun mostCommonWord(paragraph: String, banned: Array<String>): String {
+//
+//}
---
+++ src/main/kotlin/leetcode/q75/875. Koko Eating Bananas.kt
@@ -0,0 +1,34 @@
+package src.main.kotlin.leetcode.q75
+
+import java.util.*
+import kotlin.math.ceil
+import kotlin.math.min
+
+fun main(){
+    val scanner= Scanner(System.`in`)
+    val n=scanner.nextInt()
+    val numArray=IntArray(n)
+    for (i in 0..<n){
+        numArray[i]=scanner.nextInt()
+    }
+    val h=scanner.nextInt()
+    println(minEatingSpeed(numArray,h))
+}
+
+fun minEatingSpeed(piles: IntArray, h: Int): Int {
+    var end=piles.max()
+    var start=1
+    while (start<end){
+        val mid=(start+end)/2
+        var k=0
+        for(element in piles){
+            k+=(element /mid) + if(element%mid>0) 1 else 0
+        }
+        if(k>h){
+            start=mid+1
+        }else{
+            end=mid
+        }
+    }
+    return start
+}
