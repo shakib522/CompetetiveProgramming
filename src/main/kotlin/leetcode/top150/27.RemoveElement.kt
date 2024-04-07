09a3d0e40782f3717b9f326080fe50be6d7f3688
shakib522 <shakibhasann522@gmail.com>
2024-04-08 00:00:27 +0600

    solved

---
+++ src/main/kotlin/leetcode/top150/27.RemoveElement.kt
@@ -0,0 +1,29 @@
+package src.main.kotlin.leetcode.top150
+
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    var l=0
+    var r=nums.size-1
+    while(l<=r){
+        if(nums[l]==`val`&&nums[r]!=`val`){
+            val temp=nums[l]
+            nums[l]=nums[r]
+            nums[r]=temp
+        }
+        if (nums[l]!=`val`){
+            l++
+        }
+        if(nums[r]==`val`){
+            r--
+        }
+
+    }
+    var ans=0
+    for (i in nums.indices){
+        if(nums[i]!=`val`){
+            ans++
+        }else{
+            break
+        }
+    }
+    return ans
+}
