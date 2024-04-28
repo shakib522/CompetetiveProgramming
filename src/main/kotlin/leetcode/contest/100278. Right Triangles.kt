17b4561d55389eedc3020df4b686b45b525d701a
shakib522 <shakibhasann522@gmail.com>
2024-04-28 07:12:29 +0600

    numberOfRightTriangles with the same colour solved

--- src/main/kotlin/leetcode/contest/100278. Right Triangles.kt
+++ src/main/kotlin/leetcode/contest/100278. Right Triangles.kt
@@ -1,49 +1,37 @@
 package src.main.kotlin.leetcode.contest
 
-import kotlin.math.max
-
 
 fun numberOfRightTriangles(grid: Array<IntArray>): Long {
     var ans=0L
+    val row=IntArray(grid.size)
+    val col=IntArray(grid[0].size)
+
+    for (i in grid.indices){
+        var rowCount=0
+        for (j in grid[i].indices){
+            if (grid[i][j]==1){
+                rowCount++
+            }
+        }
+        row[i]=rowCount
+    }
+
+    for (i in grid[0].indices){
+        var colCount=0
+        for (j in grid.indices){
+            if(grid[j][i]==1){
+                colCount++
+            }
+        }
+        col[i]=colCount
+    }
+
     for (i in grid.indices){
         for (j in grid[i].indices){
             if (grid[i][j]==1){
-                ans+=countOne(i,j,grid)
+                ans+=(row[i]-1)*(col[j]-1)
             }
         }
     }
     return ans
-}
-
-fun countOne(i:Int,j:Int,grid: Array<IntArray>):Long{
-    var right=-1L
-    var down=-1L
-    var up=-1L
-    var left=-1L
-    for (col in j..<grid[i].size){
-        if (grid[i][col]==1){
-            right++
-        }
-    }
-    for (row in i..<grid.size){
-        if (grid[row][j]==1){
-            down++
-        }
-    }
-
-    for (row in i downTo 0){
-        if (grid[row][j]==1){
-            up++
-        }
-    }
-    for (col in j downTo 0){
-        if (grid[i][col]==1){
-            left++
-        }
-    }
-
-    val ans= max(0,right*down)+ max(0,right*up)
-    val ans2= max(0,left*down)+ max(0,left*up)
-
-    return ans+ans2
 }
