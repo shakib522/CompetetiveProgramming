109e600ad013aa66d262f2ab58fe861fba809835
shakib522 <shakibhasann522@gmail.com>
2024-06-20 09:51:08 +0600

    215.Kth Largest Element in An Array solved

---
+++ src/main/kotlin/leetcode/q75/215.Kth Largest Element in An Array.kt
@@ -0,0 +1,30 @@
+package src.main.kotlin.leetcode.q75
+
+import java.util.*
+import kotlin.Comparator
+import kotlin.math.max
+
+fun main() {
+    val scan= Scanner(System.`in`)
+    val n = scan.nextInt()
+    val ara= IntArray(n)
+    for (i in 0..<n){
+        ara[i]=scan.nextInt()
+    }
+    val k=scan.nextInt()
+    findKthLargest(ara,k)
+}
+
+fun findKthLargest(nums: IntArray, k: Int): Int {
+    val comparator = Comparator<Int>{p1,p2 ->
+        p2-p1
+    }
+    val pq=PriorityQueue(comparator)
+    nums.forEach {
+        pq.offer(it)
+    }
+    for (i in 1..<k){
+        pq.poll()
+    }
+    return pq.poll()
+}
