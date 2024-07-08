3730eb5b76c602581b7c4ce10f2f0740850e2390
shakib522 <shakibhasann522@gmail.com>
2024-07-08 20:17:06 +0600

    CountingRooms solved

---
+++ src/main/kotlin/cses/CountingRooms.java
@@ -0,0 +1,68 @@
+package src.main.kotlin.cses;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class CountingRooms {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int m=scanner.nextInt();
+        scanner.nextLine();
+        ArrayList<String> square = new ArrayList<>();
+        for (int i=0;i<n;i++){
+            String inp=scanner.nextLine();
+            square.add(i,inp);
+        }
+        int[][] vis = new int[n][m];
+        var cnt=0;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if(square.get(i).charAt(j)=='.'){
+                    if (vis[i][j]==0){
+                        cnt++;
+                        bfs(vis,square,i,j);
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+    public static void bfs(int[][]vis,ArrayList<String> square,int f,int s){
+        vis[f][s]=1;
+        Queue<Pairs> queue = new LinkedList<>();
+        queue.add(new Pairs(f,s));
+        while (!queue.isEmpty()){
+            Pairs val=queue.poll();
+            int i=val.first;
+            int j=val.second;
+            if (i<square.size()-1&& square.get(i+1).charAt(j)=='.' && vis[i+1][j]==0){
+                queue.add(new Pairs(i+1,j));
+                vis[i+1][j]=1;
+            }
+            if (i>0 && square.get(i-1).charAt(j)=='.' && vis[i-1][j]==0){
+                queue.add(new Pairs(i-1,j));
+                vis[i-1][j]=1;
+            }
+            if (j>0 && square.get(i).charAt(j-1)=='.'&& vis[i][j-1]==0){
+                queue.add(new Pairs(i,j-1));
+                vis[i][j-1]=1;
+            }
+            if (j< square.get(0).length()-1 && square.get(i).charAt(j+1)=='.'&& vis[i][j+1]==0) {
+                queue.add(new Pairs(i,j+1));
+                vis[i][j+1]=1;
+            }
+        }
+
+    }
+}
+class Pairs{
+    int first;
+    int second;
+    Pairs(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
---
+++ src/main/kotlin/hackerrank/DetectCycleInDirectedGraph.java
@@ -0,0 +1,10 @@
+package src.main.kotlin.hackerrank;
+
+import java.util.Arrays;
+
+public class DetectCycleInDirectedGraph {
+    public static void main(String[] args) {
+        int [] v = new int[10];
+        System.out.println(Arrays.toString(v));
+    }
+}
